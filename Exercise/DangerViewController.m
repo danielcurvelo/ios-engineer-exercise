5f31d0eadf537d3ddb7ff8e7945fc309d65c30e2
Daniel Curvelo <daniel.curvelo@hotmail.com>
2015-06-05 22:13:00 -0600

    Setting Minimun and Maximun values for sliders and activated alarms

--- Exercise/DangerViewController.m
+++ Exercise/DangerViewController.m
@@ -7,16 +7,51 @@
 //
 
 #import "DangerViewController.h"
+#import "SettingsViewController.h"
+#import "AlarmViewController.h"
+#import "AlarmController.h"
+#import "Alarm.h"
 
 @interface DangerViewController()
 @property (nonatomic, weak) IBOutlet UISlider *dangerSlider;
+@property (nonatomic, assign) CGFloat thresholdValue;
+@property (nonatomic, assign) BOOL isToggleOn;
 @end
 
 @implementation DangerViewController
 #pragma mark -
 #pragma mark Actions
+
+-(void)viewDidLoad
+{
+    
+    self.dangerSlider.minimumValue = 0.0;
+    self.dangerSlider.maximumValue = 1.0;
+
+}
+
 - (IBAction)dangerSliderValueChanged:(UISlider *)sender
 {
+    
+    NSLog(@"Slider Value: %f comparing to Threshold: %f",sender.value, [AlarmController sharedInstance].thresholdValue);
+    
     // TODO: Change the danger value.
+    if (sender.value >= [AlarmController sharedInstance].thresholdValue) {
+        NSLog(@"You're in Danger!");
+        [[NSNotificationCenter defaultCenter] postNotificationName:AlarmDidActivateNotification object:nil];
+        if (!isAlarmActive()) {
+            activateAlarm();
+        }
+    }
+    else
+    {
+        [[NSNotificationCenter defaultCenter] postNotificationName:AlarmDidDeactivateNotification object:nil];
+        if (isAlarmActive()) {
+            deactivateAlarm();
+        }
+    }
+    
 }
+
+
 @end
