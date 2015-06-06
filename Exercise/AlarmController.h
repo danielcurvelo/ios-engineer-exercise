b06ad61bc7c9496f1f52535502e31bbf2be78665
Daniel Curvelo <daniel.curvelo@hotmail.com>
2015-06-05 22:09:22 -0600

    Added ModelController
    Created SharedInstance and Methods to handle notifications

---
+++ Exercise/AlarmController.h
@@ -0,0 +1,13 @@
+//
+//  AlarmController.h
+//  Exercise
+//
+//  Created by Mac User on 6/5/15.
+//  Copyright (c) 2015 Ender Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AlarmController : NSObject
+
+@end
---
+++ Exercise/AlarmController.m
@@ -0,0 +1,13 @@
+//
+//  AlarmController.m
+//  Exercise
+//
+//  Created by Mac User on 6/5/15.
+//  Copyright (c) 2015 Ender Labs. All rights reserved.
+//
+
+#import "AlarmController.h"
+
+@implementation AlarmController
+
+@end
