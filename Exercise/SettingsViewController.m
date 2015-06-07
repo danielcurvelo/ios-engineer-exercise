74a8baad93c4cfb8908d3789666f1a02f89a2e7c
Daniel Curvelo <daniel.curvelo@hotmail.com>
2015-06-07 11:14:13 -0600

    Comments and refactoring
    Added comments and refactored the toogleValue getter property.

--- Exercise/SettingsViewController.m
+++ Exercise/SettingsViewController.m
@@ -21,12 +21,13 @@
 
 -(void)viewDidLoad
 {
+    // MARK: Setting minimum/maximun values and defaults.
     [super viewDidLoad];
     
     self.alarmThresholdSlider.minimumValue = 0.0;
     self.alarmThresholdSlider.maximumValue = 1.0;
     self.alarmThresholdSlider.value = [AlarmController sharedInstance].thresholdValue;
-    self.alarmEnabledSwitch.on = [AlarmController sharedInstance].toogleValue;
+    self.alarmEnabledSwitch.on = [AlarmController sharedInstance].isAlarmEnabled;
 }
 
 - (IBAction)done:(id)sender
@@ -37,15 +38,14 @@
 - (IBAction)alarmThresholdSliderValueChanged:(UISlider *)sender
 {
     // TODO: Change the alarm activation threshold.
-    
     [[NSNotificationCenter defaultCenter] postNotificationName:thresholdNote object:nil userInfo:@{value:@(sender.value)}];
-    
+    // MARK: Done.
 }
 
 - (IBAction)alarmEnabledSwitchValueChanged:(UISwitch *)sender
 {
     // TODO: Toggle whether the alarm is enabled.
-    
     [[NSNotificationCenter defaultCenter] postNotificationName:toggleNote object:nil userInfo:@{on:@(sender.on)}];
+    // MARK: Done.
 }
 @end
