ac1050c3c258f067ef868f6925eb57cd412d4be7
Daniel Curvelo <daniel.curvelo@hotmail.com>
2015-06-07 11:07:43 -0600

    Comments/Pragma marks, static variables, defaults, launches
    -Added Comments and Pragma marks
    -Created Methods to keep track of launches and to set default values according to the number of launches.

--- Exercise/AlarmController.m
+++ Exercise/AlarmController.m
@@ -9,12 +9,18 @@
 #import "AlarmController.h"
 #import "Alarm.h"
 
-NSString * thresholdNote = @"threshold";
-NSString * toggleNote = @"toggle";
-NSString * value = @"value";
-NSString * on = @"on";
+static NSString * const thresholdNote = @"thresholdNote";
+static NSString * const toggleNote = @"toggleNote";
+static NSString * const value = @"value";
+static NSString * const on = @"on";
+static NSString * const appLaunchesKey = @"launches";
+static NSString * const switchStatusKey = @"switchStatus";
+static NSString * const thresholdStatusKey = @"thresholdStatus";
 
 @implementation AlarmController
+
+#pragma mark -
+#pragma mark Initializing SharedInstance
 
 +(instancetype)sharedInstance
 {
@@ -22,49 +28,78 @@ NSString * on = @"on";
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         sharedInstance = [AlarmController new];
-        sharedInstance.thresholdValue = 0.9;
         sharedInstance.sliderValue = 0.5;
-        sharedInstance.toogleValue = YES;
+        [sharedInstance setDefaultValues];
         [sharedInstance registerNotifications];
     });
     return sharedInstance;
-    
 }
 
--(void)registerNotifications
+#pragma mark -
+#pragma mark Setting Defaults
+
+-(void)incrementAppLaunches:(int)launch
 {
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(thresholdValueDidChange:) name:thresholdNote object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(toggleValueDidChange:) name:toggleNote object:nil];
-
+    // MARK: Keeping track of App Launches.
+    self.launches = [NSNumber numberWithInt:launch + 1];
+    [[NSUserDefaults standardUserDefaults] setObject:self.launches forKey:appLaunchesKey];
 }
+
+-(void)setDefaultValues
+{
+    // MARK: Using the app launches to know which default values to set.
+    self.launches = [[NSUserDefaults standardUserDefaults] objectForKey:appLaunchesKey];
+    if (self.launches > 0) {
+        self.isAlarmEnabled = [[NSUserDefaults standardUserDefaults] boolForKey:switchStatusKey];
+        CGFloat savedThresholdValue = [[[NSUserDefaults standardUserDefaults] objectForKey:thresholdStatusKey] floatValue];
+        self.thresholdValue = savedThresholdValue;
+    }
+    else
+    {
+        self.isAlarmEnabled = YES;
+        self.thresholdValue = 0.9;
+    }
+}
+
+#pragma mark -
+#pragma mark Data Manipulation
 
 - (void)thresholdValueDidChange:(NSNotification *)note
 {
+    // MARK: Changing and saving the threshold value.
     self.thresholdValue = [note.userInfo[value] floatValue];
-    //    NSLog(@"new value: %f",self.thresholdValue);
-    
+    [[NSUserDefaults standardUserDefaults] setObject:note.userInfo[value] forKey:thresholdStatusKey];
 }
 
 - (void)toggleValueDidChange:(NSNotification *)note
 {
-    self.toogleValue = [note.userInfo[on] boolValue];
-    
-    NSNumber *alarmSwitchConvertion = [[NSNumber alloc]initWithBool:self.toogleValue];
-    [[NSUserDefaults standardUserDefaults] setObject:alarmSwitchConvertion forKey:@"isActivated"];
+    // MARK: Changing and saving the Switch valuee.
+    self.isAlarmEnabled = [note.userInfo[on] boolValue];
+    [[NSUserDefaults standardUserDefaults] setBool:self.isAlarmEnabled forKey:switchStatusKey];
 
-    if (self.toogleValue == YES) {
+    if (self.isAlarmEnabled) {
         [[NSNotificationCenter defaultCenter] postNotificationName:AlarmDidActivateNotification object:nil];
-
     }
     else
     {
         [[NSNotificationCenter defaultCenter] postNotificationName:AlarmDidDeactivateNotification object:nil];
-
     }
+}
+
+#pragma mark -
+#pragma mark Notifications
+
+-(void)registerNotifications
+{
+    // MARK: Adding Observers to track threshold and switch values.
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(thresholdValueDidChange:) name:thresholdNote object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(toggleValueDidChange:) name:toggleNote object:nil];
+    
 }
 
 -(void)dealloc
 {
+    // MARK: Removing the Observers once the object has been deallocated from memory.
     [[NSNotificationCenter defaultCenter] removeObserver:self name:thresholdNote object:nil];
     [[NSNotificationCenter defaultCenter] removeObserver:self name:toggleNote object:nil];
 }
